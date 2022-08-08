91ad93b8dbd36aeb9ba2503566cf70b1329b92fa
정정욱 <jeonguk29@naver.com>
2022-08-08 12:48:49 +0000

    실시간 검색어 파일로 저장 셀레니움 활용해서 다시 해보자 네이버 말고 구름으로

--- index.py
+++ index.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python3
-# Anchor extraction from HTML document
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-response =  urlopen('https://en.wikipedia.org/wiki/Main_Page') # url 주소를 열어서 response에 주소를 담겠다   아래 코드와 동일 
-# with urlopen('https://en.wikipedia.org/wiki/Main_Page') as response: 
-soup = BeautifulSoup(response, 'html.parser') # 주소를 넣어주고 html.parser 을 이용해 soup 담아줌 
-for anchor in soup.find_all('a'): # 모든 a 태그를 찾아서 
-    print(anchor.get('href', '/')) #href 가지고 와서 프린트 해라 
+response = urlopen('https://www.naver.com/')
+soup = BeautifulSoup(response, 'html.parser')
+i = 1
+f = open("새파일.txt", 'w')
+for anchor in soup.select("span.ah_k"):
+    data = str(i) + "위 : " + anchor.get_text() + "\n"
+    i = i + 1
+    f.write(data)
+f.close()
